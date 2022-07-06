43d1938d6dbd3001527bedefb2555f8c0fbc058d
Opifeni Emmanuel <emmideric@gmail.com>
2022-07-06 06:35:15 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,28 +7,15 @@
  */
 int main(void)
 {
-	int c, i;
+	int i;
 	char p[10] = "_putchar";
 	char t;
 
-	/*loop through the array to print each character*/
 	for (i = 0; i < 10; i++)
 	{
 		t = p[i];
-		c = _putchar(t);
-		putchar(c);
+		 _putchar(t);
 	}
 	putchar('\n');
 	return (0);
-}
-
-/**
- * _putchar - returns the ASCII integer equivalent value of a character
- * @c: the character to typecast to its ASCII integer value
- *
- * Return: the ASCII integer equivalent of c
- */
-int _putchar(char c)
-{
-	return ((int)c);
 }
