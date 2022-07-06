e28eaf9b73ce355d89848f62de8d32de9e723885
Opifeni Emmanuel <emmideric@gmail.com>
2022-07-06 05:46:48 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,4 @@
-#include <stdio.h>
-#include <unistd.h>
-
-/**
- * _putchar - function prototype that will take one argument
- * @char: the argument will be of type character
- *
- * Return: ASCII integer value of the argument..
- */
-int _putchar(char);
+#include "main.h"
 
 /**
  * main - Entry point
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - function prototype that will take one argument
+ * @char: the argument will be of type character
+ *
+ * Return: ASCII integer value of the argument..
+ */
+int _putchar(char);
